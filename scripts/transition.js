f45e10ddad37944ccffc08d544a397962fc2d675
1zumink <ilangforgames21@gmail.com>
2026-04-28 13:03:55 +0300

    фикс

--- scripts/transition.js
+++ scripts/transition.js
@@ -6,8 +6,12 @@ window.addEventListener("load", function () {
 document.querySelectorAll("a").forEach((link) => {
   link.addEventListener("click", function (e) {
     const href = link.getAttribute("href");
+    const isExternal =
+      href?.startsWith("http") ||
+      href?.startsWith("mailto:") ||
+      href?.startsWith("tel:");
 
-    if (href && !href.startsWith("#") && !link.target) {
+    if (href && !href.startsWith("#") && !isExternal && !link.target) {
       e.preventDefault();
       document.body.classList.remove("fade-in");
       document.body.classList.add("fade-out");
